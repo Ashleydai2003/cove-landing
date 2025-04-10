73d931c93aa5925d27200e476b446c9c392b6b2f
Ashley <ashleydai2003@gmail.com>
2025-04-09 23:11:48 -0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Cove",
   description: "plug back into community.",
-  icons: {
-    icon: '/favicon.svg',
-  },
 };
 
 export default function RootLayout({

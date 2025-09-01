1b9a6fa04b973acca14bc9492ac1ff7a9298c528
Nina Boord <86579493+ninaboord@users.noreply.github.com>
2025-09-01 11:20:46 -0700

    lower

--- src/app/api/notion/route.ts
+++ src/app/api/notion/route.ts
@@ -66,7 +66,7 @@ export async function POST(request: Request) {
           rich_text: [
             {
               text: {
-                content: body.city
+                content: body.city.toLowerCase()
               }
             }
           ]
